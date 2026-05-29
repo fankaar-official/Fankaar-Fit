0d8f290287b1e1bd5a82bca88a85e3029f098667
Fankaar Developer <fankaar@example.com>
2026-05-29 18:00:20 +0500

    Fix auth login to render manual form if shop is missing

--- app/routes/auth.login.jsx
+++ app/routes/auth.login.jsx
@@ -1,6 +1,62 @@
+import { useState } from "react";
+import { json } from "@remix-run/node";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import {
+  AppProvider as PolarisAppProvider,
+  Button,
+  Card,
+  FormLayout,
+  Page,
+  Text,
+  TextField,
+} from "@shopify/polaris";
+import polarisTranslations from "@shopify/polaris/locales/en.json";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+
 import { login } from "../shopify.server";
 
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+
 export const loader = async ({ request }) => {
-  await login(request);
-  return null;
+  const loginErrors = await login(request);
+  return json({ errors: loginErrors || {}, polarisTranslations });
 };
+
+export const action = async ({ request }) => {
+  const loginErrors = await login(request);
+  return json({ errors: loginErrors || {} });
+};
+
+export default function Auth() {
+  const loaderData = useLoaderData();
+  const actionData = useActionData();
+  const [shop, setShop] = useState("");
+  const { errors } = actionData || loaderData || {};
+
+  return (
+    <PolarisAppProvider i18n={loaderData?.polarisTranslations}>
+      <Page>
+        <Card>
+          <Form method="post">
+            <FormLayout>
+              <Text variant="headingMd" as="h2">
+                Log in
+              </Text>
+              <TextField
+                type="text"
+                name="shop"
+                label="Shop domain"
+                helpText="example.myshopify.com"
+                value={shop}
+                onChange={setShop}
+                autoComplete="on"
+                error={errors?.shop}
+              />
+              <Button submit>Log in</Button>
+            </FormLayout>
+          </Form>
+        </Card>
+      </Page>
+    </PolarisAppProvider>
+  );
+}
